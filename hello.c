1d34156ebb1bec8071840df48e67a4cc5b61b436
happydracula <dellwyntennison@gmail.com>
2023-09-28 09:12:49 +0000

    first commit

---
+++ hello.c
@@ -0,0 +1,27 @@
+#include "packet.h"
+#include <linux/bpf.h>
+#include<linux/ip.h>
+#include <bpf/bpf_endian.h>
+#include <linux/if_ether.h>
+#include <arpa/inet.h>
+#include <bpf/bpf_helpers.h>
+
+SEC("xdp")
+int hello(struct xdp_md *ctx) {
+/* long protocol = lookup_protocol(ctx);
+ if (protocol == 1) // ICMP
+ {
+ bpf_printk("Hello ping\n");
+ }
+ return XDP_PASS;*/
+unsigned int source=lookup_source(ctx);
+if(source==0x0101a8c0){
+	bpf_printk("Dropped packet from source:%pI4",&source);
+	return XDP_DROP;
+}
+else{
+	 bpf_printk("Passed packet from source:%pI4",&source);
+        return XDP_PASS;
+}
+}
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
---
+++ packet.h
@@ -0,0 +1,80 @@
+
+#include <linux/bpf.h>
+#include<linux/ip.h>
+#include <bpf/bpf_endian.h>
+#include <linux/if_ether.h>
+#include <arpa/inet.h>
+#include <bpf/bpf_helpers.h>
+#define ETH_P_IP        0x0800
+#define TC_ACT_UNSPEC       (-1)
+#define TC_ACT_OK		    0
+#define TC_ACT_RECLASSIFY	1
+#define TC_ACT_SHOT		    2
+
+#define ETH_P_IP	0x0800		
+#define ICMP_PING 8
+
+#define ETH_ALEN 6
+#define ETH_HLEN 14
+
+#define IP_SRC_OFF (ETH_HLEN + offsetof(struct iphdr, saddr))
+#define IP_DST_OFF (ETH_HLEN + offsetof(struct iphdr, daddr))
+
+#define ICMP_CSUM_OFF (ETH_HLEN + sizeof(struct iphdr) + offsetof(struct icmphdr, checksum))
+#define ICMP_TYPE_OFF (ETH_HLEN + sizeof(struct iphdr) + offsetof(struct icmphdr, type))
+#define ICMP_CSUM_SIZE sizeof(__u16)
+
+// Returns the protocol byte for an IP packet, 0 for anything else
+// static __always_inline unsigned char lookup_protocol(struct xdp_md *ctx)
+unsigned char lookup_protocol(struct xdp_md *ctx)
+{
+    unsigned char protocol = 0;
+
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+    struct ethhdr *eth = data;
+    if (data + sizeof(struct ethhdr) > data_end)
+        return 0;
+
+    // Check that it's an IP packet
+    if (bpf_ntohs(eth->h_proto) == ETH_P_IP)
+    {
+        // Return the protocol of this packet
+        // 1 = ICMP
+        // 6 = TCP
+        // 17 = UDP        
+        struct iphdr *iph = data + sizeof(struct ethhdr);
+        if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) <= data_end){
+            protocol = iph->protocol;
+	    bpf_printk("\nSource Addr %pI4 \n",&iph->saddr);
+	   bpf_printk("\nDestination Addr %pI4 \n",&iph->daddr);
+	}
+    }
+    return protocol;
+}
+unsigned int lookup_source(struct xdp_md *ctx)
+{
+
+
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+    struct ethhdr *eth = data;
+    if (data + sizeof(struct ethhdr) > data_end)
+        return 0;
+
+    // Check that it's an IP packet
+    if (bpf_ntohs(eth->h_proto) == ETH_P_IP)
+    {
+        // Return the protocol of this packet
+        // 1 = ICMP
+        // 6 = TCP
+        // 17 = UDP
+        struct iphdr *iph = data + sizeof(struct ethhdr);
+        if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) <= data_end){
+
+    //        bpf_printk("\nSource Addr Parsed:%pI4 \n",&iph->saddr);
+	    return iph->saddr;
+        }
+    }
+    return 0;
+}
