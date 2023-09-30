1e9e2f91a9a7602e38087ba98250dcbe2520057d
happydracula <dellwyntennison@gmail.com>
2023-09-30 11:44:53 +0000

    added basic map to count packets

--- hello.c
+++ hello.c
@@ -6,6 +6,13 @@
 #include <linux/if_ether.h>
 #include <arpa/inet.h>
 #include <bpf/bpf_helpers.h>
+struct {
+        __uint(type, BPF_MAP_TYPE_ARRAY);
+        __type(key, __u32);
+        __type(value, long);
+        __uint(max_entries, 2);
+} packetmap SEC(".maps");
+
 SEC("xdp")
 int hello(struct xdp_md *ctx) {
 /* long protocol = lookup_protocol(ctx);
@@ -16,17 +23,31 @@ int hello(struct xdp_md *ctx) {
  return XDP_PASS;*/
 struct iphdr*iph=retrieve_ip(ctx);
 unsigned int source=lookup_source(iph);
+__u32 key;
+long* value;
 if(source==0x0101a8c0){
+             key=0;
 	bpf_printk("Dropped packet from source:%pI4",&source);
+	 value = bpf_map_lookup_elem(&packetmap, &key);
+                if (value)
+                        *value += 1;
 	return XDP_DROP;
 }
 else{
 	unsigned int drop=detect_malicious(ctx);
 	if(drop==1){
+		key=0;
+	 value = bpf_map_lookup_elem(&packetmap, &key);
+                if (value)
+                        *value += 1;
 		bpf_printk("Dropped malicious packet from source:%pI4",&source);
 		return XDP_DROP;
          } 
 	bpf_printk("Passed packet from source:%pI4",&source);
+	key=1;
+	 value = bpf_map_lookup_elem(&packetmap, &key);
+                if (value)
+                        *value += 1;
         return XDP_PASS;
 }
 }
--- user_load.c
+++ user_load.c
@@ -12,6 +12,26 @@
 #include <xdp/libxdp.h>
 static int ifindex;
 struct xdp_program *prog = NULL;
+static void poll_stats(int map_fd, int interval)
+{
+    long value, prev=0, total_pkts;
+    int i, key = 0;
+
+    
+
+    while (1) {
+        long sum = 0;
+        sleep(interval);
+        assert(bpf_map_lookup_elem(map_fd, &key, &value) == 0);
+        sum=value-prev;
+        if (sum) {
+            total_pkts += sum;
+            printf("total dropped %10lu, %10lu pkt/s\n",
+                   total_pkts, sum / interval);
+        }
+        prev=value;
+    }
+}
 int main(int argc, char *argv[])
 {
     int prog_fd, map_fd, ret;
@@ -43,7 +63,13 @@ int main(int argc, char *argv[])
         printf("Error, Set xdp fd on %d failed\n", ifindex);
         return ret;
     }
-
+     bpf_obj = xdp_program__bpf_obj(prog);
+    map_fd = bpf_object__find_map_fd_by_name(bpf_obj, "packetmap");
+    if (map_fd < 0) {
+        printf("Error, get map fd from bpf obj failed\n");
+        return map_fd;
+    }
+    poll_stats(map_fd, 2);
     /* Find the map fd from the bpf object */
   
     return 0;
